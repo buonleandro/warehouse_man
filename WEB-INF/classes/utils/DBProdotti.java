ebd71b8ab5fc7373b7de6e813398cdc43e76022a
buonleandro <assassincreeddark@gmail.com>
2017-01-06 00:37:09 +0100

    Logica 5 gennaio

--- WEB-INF/classes/utils/DBProdotti.java
+++ WEB-INF/classes/utils/DBProdotti.java
@@ -17,15 +17,14 @@ public class DBProdotti {
 	private String dbName = "wcDatabase";
 	private String tableName = "prodotti";
 	private Properties userInfo;
-	private int i = 0;
-
+	private int id;
 
 	public DBProdotti() {
 		userInfo = new Properties();
 		userInfo.put("user", username);
 		userInfo.put("pass", password);
 	}
-	
+
 	protected Connection getConnection()
 			throws Exception {
 		Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
@@ -33,16 +32,16 @@ public class DBProdotti {
 		Connection connection = DriverManager.getConnection(dbUrl, userInfo);
 		return(connection);
 	}
-	
+
 	public void createDatabase() {
-		
+
 		Prodotto[] prodotti = {
-				new Prodotto("a", "a", "images/trasparentone.png", 1.0, "a", "a","a","a", "?a", 1)
+				new Prodotto(0, "a", "a", "images/trasparentone.png", 1.0, "a", "a","a","a", "?a", 1)
 		};
 
 		try {
 			Connection connection = getConnection();
-								
+
 			Statement statement = connection.createStatement();
 			String format = "VARCHAR(80)";
 			String lnkimg = "VARCHAR(256)";
@@ -54,12 +53,13 @@ public class DBProdotti {
 							"lnkimg %s, lnk %s NOT NULL, num INT NOT NULL)",
 							tableName, format, format, format, format, format, format2, lnkimg, format);
 			statement.execute(table);
-			
+
 			String query = String.format("INSERT INTO %s VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", tableName);
 			PreparedStatement inserter =
 					connection.prepareStatement(query);
 			for(Prodotto p:prodotti){
-				inserter.setInt(1, i++);
+				inserter.setInt(1, p.getId());
+				//System.out.println(p.getId()+"Sono in createDB");
 				inserter.setString(2, p.getNome());
 				inserter.setString(3, p.getSN());
 				inserter.setDouble(4, p.getPrezzo());
@@ -73,31 +73,32 @@ public class DBProdotti {
 				inserter.executeUpdate();
 			}
 			inserter.close();
-			
+
 			connection.close();
 			System.out.println("Database creato");
 		} catch (SQLException sqlException) {
-	        if (sqlException.getErrorCode() == 1007) {
-	            System.out.println("Errore " + sqlException.getMessage());
-	        } else {
-	            sqlException.printStackTrace();
-	        }
-	    } catch (ClassNotFoundException e) {
-	    } catch (Exception e1) {
+			if (sqlException.getErrorCode() == 1007) {
+				System.out.println("Errore " + sqlException.getMessage());
+			} else {
+				sqlException.printStackTrace();
+			}
+		} catch (ClassNotFoundException e) {
+		} catch (Exception e1) {
 			e1.printStackTrace();
 		}
 	}
-	
+
 	public List<Prodotto> showAll() {
-		
+
 		List<Prodotto> prods = new ArrayList<Prodotto>();
-		
+
 		try {
 			Connection connection = getConnection();
 			Statement statement = connection.createStatement();
-			String query = "SELECT * FROM prodotti";
+			String query = "SELECT * FROM prodotti ORDER BY id ASC";
 			ResultSet resultSet = statement.executeQuery(query);
 			while(resultSet.next()) {
+				int id = resultSet.getInt("id");
 				String nome = resultSet.getString("nome");
 				String sn = resultSet.getString("SN");
 				Double prezzo = resultSet.getDouble("prezzo");
@@ -108,8 +109,10 @@ public class DBProdotti {
 				String lnkimg = resultSet.getString("lnkimg");
 				int num = resultSet.getInt("num");
 				String lnk = resultSet.getString("lnk");
-				Prodotto prod = new Prodotto(nome, sn, lnkimg, prezzo, tipo, marca, db, dl, lnk, num);
-				prods.add(prod);
+				if(num!=0){
+					Prodotto prod = new Prodotto(id, nome, sn, lnkimg, prezzo, tipo, marca, db, dl, lnk, num);
+					prods.add(prod);
+				}
 			}
 			connection.close();
 		} catch (SQLException e) {
@@ -120,40 +123,138 @@ public class DBProdotti {
 		return prods;
 	}
 	
-	public void aggiungiProdotto(String sn, String nome, String tipo, String marca, double prezzo, String desb, String desl, int pezzi, String img) {
+	public Prodotto showSingle(int id) {
+
+		Prodotto prod = null;
 		
+		try {
+			Connection connection = getConnection();
+			Statement statement = connection.createStatement();
+			String query = "SELECT * FROM prodotti WHERE id="+id;
+			ResultSet resultSet = statement.executeQuery(query);
+			resultSet.next();
+				String nome = resultSet.getString("nome");
+				String sn = resultSet.getString("SN");
+				Double prezzo = resultSet.getDouble("prezzo");
+				String tipo = resultSet.getString("tipo");
+				String marca = resultSet.getString("marca");
+				String db = resultSet.getString("db");
+				String dl = resultSet.getString("dl");
+				String lnkimg = resultSet.getString("lnkimg");
+				int num = resultSet.getInt("num");
+				String lnk = resultSet.getString("lnk");
+				prod = new Prodotto(id, nome, sn, lnkimg, prezzo, tipo, marca, db, dl, lnk, num);
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return prod;
+	}
+
+	public void aggiungiProdotto(String sn, String nome, String tipo, String marca, double prezzo, String desb, String desl, int pezzi, String img) {
+
 		try{
 			Connection connection = getConnection();
+			Statement statement2 = connection.createStatement();
+			String query2 = "SELECT MAX(id) FROM prodotti";
+			ResultSet idM = statement2.executeQuery(query2);
 			String query = String.format("INSERT INTO %s VALUES(?,?,?,?,?,?,?,?,?,?,?)", tableName);
 			PreparedStatement inserter =
 					connection.prepareStatement(query);
-				inserter.setInt(1, i++);
-				inserter.setString(2, nome);
-				inserter.setString(3, sn);
-				inserter.setDouble(4, prezzo);
-				inserter.setString(5, tipo);
-				inserter.setString(6, marca);
-				inserter.setString(7, desb);
-				inserter.setString(8, desl);
-				inserter.setString(9, img);
-				inserter.setString(10, "?"+sn);
-				inserter.setInt(11, pezzi);
-				inserter.toString();
-				inserter.executeUpdate();
+			while(idM.next()){
+				id=idM.getInt(1)+1;
+				//System.out.println("id del prodotto="+id);
+			}
+			inserter.setInt(1, id);
+			inserter.setString(2, nome);
+			inserter.setString(3, sn);
+			inserter.setDouble(4, prezzo);
+			inserter.setString(5, tipo);
+			inserter.setString(6, marca);
+			inserter.setString(7, desb);
+			inserter.setString(8, desl);
+			inserter.setString(9, img);
+			inserter.setString(10, "?"+sn);
+			inserter.setInt(11, pezzi);
+			inserter.executeUpdate();
 			inserter.close();
 			connection.close();
 			System.out.println("Prodotto inserito");
 		} catch (SQLException sqlException) {
-	        if (sqlException.getErrorCode() == 1007) {
-	            System.out.println("Errore! " + sqlException.getMessage());
-	        } else {
-	            sqlException.printStackTrace();
-	        }
-	    } catch (ClassNotFoundException e) {
-	    } catch (Exception e1) {
+			if (sqlException.getErrorCode() == 1007) {
+				System.out.println("Errore! " + sqlException.getMessage());
+			} else {
+				sqlException.printStackTrace();
+			}
+		} catch (ClassNotFoundException e) {
+		} catch (Exception e1) {
 			e1.printStackTrace();
 		}
-		
+
 	}
 	
+	public void aggiornaProdotto(int id, String nome, String SN, String tipo, String marca, String db, String dl, int num, double prezzo) throws Exception{
+		Connection connection = getConnection();
+		Statement statement = connection.createStatement();
+		String query =
+				String.format("SELECT * FROM %s WHERE id ="+id,
+						tableName);
+		ResultSet queryResult = statement.executeQuery(query);
+		queryResult.next();
+		String template =
+				String.format("UPDATE %s SET nome ="+(nome)+"WHERE id ="+id,
+						tableName);
+		statement.executeUpdate(template);
+		String template2 =
+				String.format("UPDATE %s SET SN ="+(SN)+"WHERE id ="+id,
+						tableName);
+		statement.executeUpdate(template2);
+		String template3 =
+				String.format("UPDATE %s SET tipo ="+(tipo)+"WHERE id ="+id,
+						tableName);
+		statement.executeUpdate(template3);
+		String template4 =
+				String.format("UPDATE %s SET marca ="+(marca)+"WHERE id ="+id,
+						tableName);
+		statement.executeUpdate(template4);
+		String template5 =
+				String.format("UPDATE %s SET db ="+(db)+"WHERE id ="+id,
+						tableName);
+		statement.executeUpdate(template5);
+		String template6 =
+				String.format("UPDATE %s SET dl ="+(dl)+"WHERE id ="+id,
+						tableName);
+		statement.executeUpdate(template6);
+		String template7 =
+				String.format("UPDATE %s SET num ="+(num)+"WHERE id ="+id,
+						tableName);
+		statement.executeUpdate(template7);
+		String template8 =
+				String.format("UPDATE %s SET lnk = ?"+(SN)+"WHERE id ="+id,
+						tableName);
+		statement.executeUpdate(template8);
+		String template9 =
+				String.format("UPDATE %s SET prezzo ="+(prezzo)+"WHERE id ="+id,
+						tableName);
+		statement.executeUpdate(template9);
+		connection.close();
+	}
+
+	public void rimuoviDalDB(int id) throws Exception{
+		Connection connection = getConnection();
+		Statement statement2 = connection.createStatement();
+		String query =
+				String.format("SELECT * FROM %s WHERE id ="+id,
+						tableName);
+		ResultSet queryResult = statement2.executeQuery(query);
+		queryResult.next();
+		String template =
+				String.format("DELETE FROM %s WHERE id ="+id,
+						tableName);
+		statement2.executeUpdate(template);
+		connection.close();
+	}
+
 }
